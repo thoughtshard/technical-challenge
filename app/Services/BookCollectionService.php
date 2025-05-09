9cd6827647f95fe2e42306adbc72adaf16c00ed4
Savriss <savriss@gmail.com>
2025-05-09 16:39:20 +0100

    Linting.

--- app/Services/BookCollectionService.php
+++ app/Services/BookCollectionService.php
@@ -1,11 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
 
 use App\Models\Book;
 use App\Models\Collector;
 
-class BookCollectionService
+Final class BookCollectionService
 {
     /**
      * @param Collector $collector
